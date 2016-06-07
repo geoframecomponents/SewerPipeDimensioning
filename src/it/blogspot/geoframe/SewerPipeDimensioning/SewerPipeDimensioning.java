047ab502a0505010fa91340f7e096cdc4e95adad
ftt01 <dallatorre.daniele@gmail.com>
2016-06-07 12:32:07 +0200

    Modified name of some variables.

--- src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
+++ src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
@@ -17,83 +17,82 @@ public class SewerPipeDimensioning {
 	private static boolean flag = false;
 	
 	/** Definition of diameter */
-	public static double D;
+	public static double diameter;
 	
 	/** Definition of fill coefficient */
-	public static double G = 0.8;
+	public static double fillCoefficient= 0.8;
 	
 	/** Definition of shear stress [Pa] */
-	public static double tau = 2.0;
+	public static double shearStress = 2.0;
 	
 	/** Definition of discharge [l/s] */
-	public static double Q = 2.0;
+	public static double discharge = 2.0;
 	
 	/** Definition of Gaucker-Strickler coefficient */
-	public static double Ks = 0.03;
+	public static double gaucklerStricklerCoefficient = 0.03;
 	
 	/** Definition of hydraulic radius */
-	public static double Rh;
+	public static double hydraulicRadius;
 	
 	/** Evaluation of theta from G=0.5*(1-cos(theta/2) */
-	private double getTheta(double G) {
-		return 2*Math.acos(1-2*G);
+	private double getFillAngle(double fillCoefficient) {
+		return 2*Math.acos(1-2*fillCoefficient);
 	}
 	
 	/** Evaluation of G (fill coefficient) from G=0.5*(1-cos(theta/2) */
-	private double getG(double theta) {
-		return 0.5 *(1-Math.cos(theta/2));
+	private double getFillCoefficient(double fillAngle) {
+		return 0.5 *(1-Math.cos(fillAngle/2));
 	}
 	
 	/** Evaluation of Rh (hydraulic radius) from Rh=D/4*(1-sin(theta)/theta) */
-	private double getRh(double D, double theta) {
-		return D/4*(1-Math.sin(theta)/theta);
+	private double getHydraulicRadius(double diameter, double fillAngle) {
+		return diameter/4*(1-Math.sin(fillAngle)/fillAngle);
 	}
 	
 	/** Evaluation of slope from slope=tau/(WSPECIFICWEIGHT*Rh) */
-	private double getSlope(double tau, double Rh) {
-		return tau/(WSPECIFICWEIGHT*Rh);
+	private double getSlope(double shearStress, double hydraulicRadius) {
+		return shearStress/(WSPECIFICWEIGHT*hydraulicRadius);
 	}
 	
 	/** Check if the minimum Slope is greater/smaller than natural Slope */
-	private double checkSlope(double minSlope, double nSlope) {
-		if  (nSlope >= minSlope) {
-			return nSlope;
-		}
-		else {
+	private double checkSlope(double minSlope, double naturalSlope) {
+		if  (naturalSlope >= minSlope) {
+			return naturalSlope;
+		} else {
 			flag = true;
 			return minSlope;
 		}
 	}
 
 	/** Evaluation of natural slope */
-	private double naturalSlope(double y1, double y2, double lenght) {
-		return (y1-y2)/lenght;
+	private double naturalSlope(double startElevation, double endElevation, double pipeLenght) {
+		return (startElevation-endElevation)/pipeLenght;
 	}
 
 	/** Evaluation of minimum slope */
 	private double minSlope() {
-		double theta = getTheta(G);
-		D = getFixedD(Q,theta,tau,Ks);
+		double fillAngle = getFillAngle(fillCoefficient);
+		diameter = getFixedDiameter(discharge,fillAngle,shearStress,gaucklerStricklerCoefficient);
 		/*! /TODO evaluation of commercial diameters */
-		Rh = getRh(D,theta);
-		return getSlope(tau, Rh);
+		hydraulicRadius = getHydraulicRadius(diameter,fillAngle);
+		return getSlope(shearStress, hydraulicRadius);
 	}
 	
 	/** Evaluation of diameter due to autocleaning */
-	private double getFixedD(double Q, double theta, double tau, double Ks) {
-		double num = Math.pow(4, 13/6);
-		double den = theta*Math.pow(1-Math.sin(theta)/theta,7/6)*Ks*Math.pow(tau/WSPECIFICWEIGHT,0.5);
-		return Math.pow(num/den,6/13);
+	private double getFixedDiameter(double discharge, double fillAngle, double shearStress, double gaucklerStricklerCoefficient) {
+		double numerator =  Math.pow(4, 13.0/6);
+		double denominator = fillAngle*Math.pow(1-Math.sin(fillAngle)/fillAngle, 7.0/6)*gaucklerStricklerCoefficient*Math.pow(shearStress/WSPECIFICWEIGHT, 0.5);
+		return Math.pow(numerator/denominator, 6.0/13);
 	}
 
 	/** Evaluation of diameter due to natural slope */
 	private double getDiameter(double slope) {
-		double theta = getTheta(G);
-		double num = Math.pow( (Q*theta)/(Ks*Math.pow(slope,0.5)), 3.0/8 );
+		double fillAngle = getFillAngle(fillCoefficient);
+		double numerator = Math.pow( (discharge*fillAngle)/(gaucklerStricklerCoefficient*Math.pow(slope,0.5)), 3.0/8);
 		// double num = (Q*Math.pow(2, 13.0/3))/(Ks*Math.pow(slope, 0.5));
-		double den = Math.pow(1-Math.sin(theta)/theta, 5.0/8);
+		double denominator = Math.pow(1-Math.sin(fillAngle)/fillAngle, 5.0/8);
 		// double den = Math.pow(1-Math.sin(theta)/theta,2.0/3)*(theta - Math.sin(theta));
-		return num/den*Math.pow(10,-9.0/8);
+		return numerator/denominator*Math.pow(10,-9.0/8);
 	}
 	
 	public static void main(String [ ] args)
@@ -101,18 +100,18 @@ public class SewerPipeDimensioning {
 		SewerPipeDimensioning test = new SewerPipeDimensioning();
 		
 		double minSlope = test.minSlope();
-		double nSlope = test.naturalSlope(y1, y2, L);
+		double naturalSlope = test.naturalSlope(startElevation, endElevation, pipeLenght);
 		
-		double slope = test.checkSlope(minSlope, nSlope);
+		double slope = test.checkSlope(minSlope, naturalSlope);
 		  
 		if (flag == false) {
 			System.out.println(slope);
-	    	 D = test.getDiameter(slope);
+	    	 diameter = test.getDiameter(slope);
 	     } 
 		System.out.println(slope);
-		System.out.println(G);
-		System.out.println(test.getTheta(slope));
-		System.out.println(D*100);
+		System.out.println(fillCoefficient);
+		System.out.println(test.getFillAngle(slope));
+		System.out.println(diameter*100);
 		// System.out.println(test.getSlope(tau, Rh));
 	}
 	
