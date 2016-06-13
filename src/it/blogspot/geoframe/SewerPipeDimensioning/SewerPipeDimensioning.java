f0d6c04319a8d1ee6a56360f84dd171de2c22e54
ftt01 <dallatorre.daniele@gmail.com>
2016-06-13 17:41:50 +0200

    Added constructors, added check to minimum excavation, change some computation methods and added use of submodules.

--- src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
+++ src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
@@ -1,36 +1,40 @@
 package it.blogspot.geoframe.SewerPipeDimensioning;
 
 import it.blogspot.geoframe.utils.GEOconstants;
+import it.blogspot.geoframe.utils.GEOgeometry;
+import it.blogspot.geoframe.hydroGeoEntities.line.*;
 
 public class SewerPipeDimensioning {
+	
+    private static double gaucklerStricklerCoefficient;
+    private static double fillCoefficient;
+    private double fillAngle;
+    private static double discharge;
+    private double hydraulicRadius;
+    private double minSlope;
+    private double pipeSlope;
+    public double diameter;
 
-	/** Definition of first node terrain elevation */
-	private static final double startElevation = 7.76;
+    private Pipe pipe;
 
-	/** Definition of second node terrain elevation */
-	private static final double endElevation = 4.65;
+    public SewerPipeDimensioning(final Pipe pipe) {
+    	this.pipe = pipe;
+		setFields();
+	}
 
-	/** Definition of pipe horizontal lenght */
-	private static final double pipeLenght = 10.0;
-	
-	/** Definition of diameter */
-	public static double diameter;
-	
-	/** Definition of fill coefficient */
-	public static double fillCoefficient= 0.8;
-	
-	/** Definition of shear stress [Pa] */
-	public static double shearStress = 2.0;
-	
-	/** Definition of discharge [l/s] */
-	public static double discharge = 2.0;
-	
-	/** Definition of Gaucker-Strickler coefficient */
-	public static double gaucklerStricklerCoefficient = 0.03;
-	
-	/** Definition of hydraulic radius */
-	public static double hydraulicRadius;
-	
+	private void setFields() {
+		gaucklerStricklerCoefficient = pipe.getGaucklerStricklerCoefficient();
+		fillCoefficient = pipe.getFillCoefficient();
+		discharge = pipe.getDischarge();
+
+		fillAngle = computeFillAngle();
+
+		pipe.setElevationEndPoint(pipe.getEndPoint().getTerrainElevation()-GEOconstants.MINIMUMEXCAVATION);
+		pipeSlope = computePipeSlope();
+		
+		minSlope = computeMinSlope();
+	}
+
 	/** 
 	 * Evaluation of theta from G=0.5*(1-cos(theta/2)
 	 */
@@ -38,14 +42,7 @@ public class SewerPipeDimensioning {
 		return 2*Math.acos(1-2*fillCoefficient);
 	}
 	
-	/** 
-	 * Evaluation of G (fill coefficient) from G=0.5*(1-cos(theta/2)
-	 */
-	private double computeFillCoefficient(double fillAngle) {
-		return 0.5 *(1-Math.cos(fillAngle/2));
-	}
-	
-	/** 
+	/**
 	 * Evaluation of Rh (hydraulic radius) from Rh=D/4*(1-sin(theta)/theta)
 	 */
 	private double computeHydraulicRadius(double fillAngle) {
@@ -53,72 +50,74 @@ public class SewerPipeDimensioning {
 	}
 	
 	/** 
-	 * Evaluation of slope from slope=tau/(WSPECIFICWEIGHT*Rh)
+	 * Evaluation of slope due to fixed shear stress from slope=tau/(WSPECIFICWEIGHT*Rh)
 	 */
-	private double computeSlope() {
-		return shearStress/(GEOconstants.WSPECIFICWEIGHT*hydraulicRadius);
-	}
-	
-	/** 
-	 * Evaluation of natural slope
-	 */
-	private double evaluateNaturalSlope() {
-		return (startElevation-endElevation)/pipeLenght;
-	}
-
-	/** 
-	 * Evaluation of minimum slope
-	 */
-	private double evaluateMinSlope() {
-		double fillAngle = computeFillAngle();
+	private double computeMinSlope() {
 		diameter = computeFixedDiameter(fillAngle);
-		/*! 
-		 * /TODO evaluation of commercial diameters
-		 */
 		hydraulicRadius = computeHydraulicRadius(fillAngle);
-		return computeSlope();
+		return GEOconstants.SHEARSTRESS/(GEOconstants.WSPECIFICWEIGHT*hydraulicRadius);
 	}
 	
-	/** 
+	/**
 	 * Evaluation of diameter due to auto-cleaning
 	 */
 	private double computeFixedDiameter(double fillAngle) {
 		final double pow1 = 13.0/6;
 		double numerator =  Math.pow(4, pow1);
 		final double pow2 = 7.0/6;
-		double denominator = fillAngle*Math.pow(1-Math.sin(fillAngle)/fillAngle, pow2)*gaucklerStricklerCoefficient*Math.pow(shearStress/GEOconstants.WSPECIFICWEIGHT, 0.5);
+		double denominator = fillAngle*Math.pow(1-Math.sin(fillAngle)/fillAngle, pow2)*gaucklerStricklerCoefficient*
+								Math.pow(GEOconstants.SHEARSTRESS/GEOconstants.WSPECIFICWEIGHT, 0.5);
 		final double pow3 = 6.0/13;
-		
+
 		return Math.pow(numerator/denominator, pow3);
 	}
 
-	/** 
-	 * Evaluation of diameter due to natural slope
+	/**
+	 * Evaluation of pipeSlope with minimum excavation
+	 */
+	private double computePipeSlope() {
+		return GEOgeometry.computeSlope(pipe.getStartPoint().getX(), pipe.getStartPoint().getY(), pipe.getStartPoint().getElevation(),
+										pipe.getEndPoint().getX(), pipe.getEndPoint().getY(), pipe.getEndPoint().getElevation());
+	}
+
+	/**
+	 * Evaluation of diameter
 	 */
 	private double computeDiameter(double slope) {
-		
 		double fillAngle = computeFillAngle();
 		final double pow1 = 3.0/8;
 		double numerator = Math.pow( (discharge*fillAngle)/(gaucklerStricklerCoefficient*Math.pow(slope,0.5)), pow1);
 		final double pow2 = 5.0/8;
 		double denominator = Math.pow(1-Math.sin(fillAngle)/fillAngle, pow2);
 		final double pow3 = -9.0/8;
-		
+
 		return numerator/denominator*Math.pow(10,pow3);
 	}
+
+	/**
+	 * Evaluation of elevation of end point due to a defined slope
+	 */
+	private double computeElevationEndPoint(double slope) {
+		return pipe.getStartPoint().getElevation()-slope*GEOgeometry.horizontalProjection(pipe.getStartPoint().getX(),
+				pipe.getStartPoint().getY(), pipe.getEndPoint().getX(), pipe.getEndPoint().getY());
+	}
+
+	/**
+	 * Evaluation of velocity
+	 */
+	private double computeVelocity() {
+		double numerator = discharge*8;
+		double denominator = diameter*(fillAngle-Math.sin(fillAngle));
+		return numerator/denominator;
+	}
 	
-	public static void main(String [ ] args)
-	{
-		SewerPipeDimensioning test = new SewerPipeDimensioning();
-		double minSlope = test.evaluateMinSlope();
-		double naturalSlope = test.evaluateNaturalSlope();
-		double slope;
-		
-		if  (naturalSlope >= minSlope) {
-			slope = naturalSlope;
+	public Pipe run() {
+		if (pipeSlope>=minSlope) {
+			pipe.buildPipe(pipe.getEndPoint().getElevation(), computeDiameter(pipeSlope), fillCoefficient, computeVelocity());
 		} else {
-			slope = minSlope;
+			pipe.buildPipe(computeElevationEndPoint(minSlope), diameter, fillCoefficient, computeVelocity());
 		}
-		diameter = test.computeDiameter(slope);
+
+		return pipe;
 	}
 }
