6963661bfbf597c6bf0bb873e2b862bd0ba4137a
ftt01 <dallatorre.daniele@gmail.com>
2016-06-13 17:44:13 +0200

    Added Licence, date and authors.

--- src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
+++ src/it/blogspot/geoframe/SewerPipeDimensioning/SewerPipeDimensioning.java
@@ -1,8 +1,35 @@
+/*
+ * GNU GPL v3 License
+ *
+ * Copyright 2015 AboutHydrology (Riccardo Rigon)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package it.blogspot.geoframe.SewerPipeDimensioning;
 
 import it.blogspot.geoframe.utils.GEOconstants;
 import it.blogspot.geoframe.utils.GEOgeometry;
 import it.blogspot.geoframe.hydroGeoEntities.line.*;
+
+/**
+*
+*
+* @author ftt01, dallatorre.daniele@gmail.com
+* @version 0.1
+* @date June 13, 2016
+* @copyright GNU Public License v3 GWH-2b4
+*/
 
 public class SewerPipeDimensioning {
 	
